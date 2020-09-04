afd377fda86a348dfed3638477bc433663636185
DimiNak <61295912+diminako@users.noreply.github.com>
2020-09-03 20:50:31 -0400

    Add files via upload

---
+++ speech_project.js
@@ -0,0 +1,214 @@
+let li1 = document.querySelector("#li_1");
+let li2 = document.querySelector("#li_2");
+let li3 = document.querySelector("#li_3");
+let li4 = document.querySelector("#li_4");
+
+let btnA = document.querySelector(".btnA");
+let btnB = document.querySelector(".btnB");
+let btnC = document.querySelector(".btnC");
+let btnD = document.querySelector(".btnD");
+let btnE = document.querySelector(".btnE");
+let btnF = document.querySelector(".btnF");
+let btnG = document.querySelector(".btnG");
+let btnH = document.querySelector(".btnH");
+let btnI = document.querySelector(".btnI");
+let btnJ = document.querySelector(".btnJ");
+let btnK = document.querySelector(".btnK");
+let btnL = document.querySelector(".btnL");
+let btnM = document.querySelector(".btnM");
+let btnN = document.querySelector(".btnN");
+let btnO = document.querySelector(".btnO");
+let btnP = document.querySelector(".btnP");
+let btnQ = document.querySelector(".btnQ");
+let btnR = document.querySelector(".btnR");
+let btnS = document.querySelector(".btnS");
+let btnT = document.querySelector(".btnT");
+let btnU = document.querySelector(".btnU");
+let btnV = document.querySelector(".btnV");
+let btnW = document.querySelector(".btnW");
+let btnX = document.querySelector(".btnX");
+let btnY = document.querySelector(".btnY");
+let btnZ = document.querySelector(".btnZ");
+
+btnA.addEventListener("click", function(){
+    li1.textContent = "Apple";
+    li2.textContent = "Actress";
+    li3.textContent = "Answer";
+    li4.textContent = "Artist";
+})
+
+btnB.addEventListener("click", function(){
+    li1.textContent = "Bow";
+    li2.textContent = "Beat";
+    li3.textContent = "Bite";
+    li4.textContent = "Boy";
+})
+
+btnC.addEventListener("click", function(){
+    li1.textContent = "Craft";
+    li2.textContent = "Class";
+    li3.textContent = "Church";
+    li4.textContent = "Clip";
+})
+
+btnD.addEventListener("click", function(){
+    li1.textContent = "Dog";
+    li2.textContent = "Doodle";
+    li3.textContent = "Daddy";
+    li4.textContent = "Deal";
+})
+
+btnE.addEventListener("click", function(){
+    li1.textContent = "Eyes";
+    li2.textContent = "Eat";
+    li3.textContent = "Egg";
+    li4.textContent = "Expert";
+})
+
+btnF.addEventListener("click", function(){
+    li1.textContent = "Fall";
+    li2.textContent = "Fish";
+    li3.textContent = "Fire";
+    li4.textContent = "Forget";
+})
+
+btnG.addEventListener("click", function(){
+    li1.textContent = "Group";
+    li2.textContent = "Good";
+    li3.textContent = "Gallop";
+    li4.textContent = "Girl";
+})
+
+btnH.addEventListener("click", function(){
+    li1.textContent = "Home";
+    li2.textContent = "Happy";
+    li3.textContent = "Horse";
+    li4.textContent = "Hair";
+})
+
+btnI.addEventListener("click", function(){
+    li1.textContent = "Icky";
+    li2.textContent = "Ice";
+    li3.textContent = "Iceberg";
+    li4.textContent = "Icon";
+})
+
+btnJ.addEventListener("click", function(){
+    li1.textContent = "Jam";
+    li2.textContent = "Jab";
+    li3.textContent = "Jack";
+    li4.textContent = "Jar";
+})
+
+btnK.addEventListener("click", function(){
+    li1.textContent = "Kabob";
+    li2.textContent = "Karma";
+    li3.textContent = "Karat";
+    li4.textContent = "Kaput";
+})
+
+btnL.addEventListener("click", function(){
+    li1.textContent = "Life";
+    li2.textContent = "Love";
+    li3.textContent = "Lever";
+    li4.textContent = "Layer";
+})
+
+btnM.addEventListener("click", function(){
+    li1.textContent = "Mommy";
+    li2.textContent = "Money";
+    li3.textContent = "Monkey";
+    li4.textContent = "Magic";
+})
+
+btnN.addEventListener("click", function(){
+    li1.textContent = "Neat";
+    li2.textContent = "Never";
+    li3.textContent = "Neptune";
+    li4.textContent = "Next";
+})
+
+btnO.addEventListener("click", function(){
+    li1.textContent = "Blah blabbity blah";
+    li2.textContent = "Blah blabbity blah";
+    li3.textContent = "Blah blabbity blah";
+    li4.textContent = "Blah blabbity blah";
+})
+
+btnP.addEventListener("click", function(){
+    li1.textContent = "Blah blabbity blah";
+    li2.textContent = "Blah blabbity blah";
+    li3.textContent = "Blah blabbity blah";
+    li4.textContent = "Blah blabbity blah";
+})
+
+btnQ.addEventListener("click", function(){
+    li1.textContent = "Blah blabbity blah";
+    li2.textContent = "Blah blabbity blah";
+    li3.textContent = "Blah blabbity blah";
+    li4.textContent = "Blah blabbity blah";
+})
+
+btnR.addEventListener("click", function(){
+    li1.textContent = "Blah blabbity blah";
+    li2.textContent = "Blah blabbity blah";
+    li3.textContent = "Blah blabbity blah";
+    li4.textContent = "Blah blabbity blah";
+})
+
+btnS.addEventListener("click", function(){
+    li1.textContent = "Blah blabbity blah";
+    li2.textContent = "Blah blabbity blah";
+    li3.textContent = "Blah blabbity blah";
+    li4.textContent = "Blah blabbity blah";
+})
+
+btnT.addEventListener("click", function(){
+    li1.textContent = "Blah blabbity blah";
+    li2.textContent = "Blah blabbity blah";
+    li3.textContent = "Blah blabbity blah";
+    li4.textContent = "Blah blabbity blah";
+})
+
+btnU.addEventListener("click", function(){
+    li1.textContent = "Blah blabbity blah";
+    li2.textContent = "Blah blabbity blah";
+    li3.textContent = "Blah blabbity blah";
+    li4.textContent = "Blah blabbity blah";
+})
+
+btnV.addEventListener("click", function(){
+    li1.textContent = "Blah blabbity blah";
+    li2.textContent = "Blah blabbity blah";
+    li3.textContent = "Blah blabbity blah";
+    li4.textContent = "Blah blabbity blah";
+})
+
+btnW.addEventListener("click", function(){
+    li1.textContent = "Blah blabbity blah";
+    li2.textContent = "Blah blabbity blah";
+    li3.textContent = "Blah blabbity blah";
+    li4.textContent = "Blah blabbity blah";
+})
+
+btnX.addEventListener("click", function(){
+    li1.textContent = "Blah blabbity blah";
+    li2.textContent = "Blah blabbity blah";
+    li3.textContent = "Blah blabbity blah";
+    li4.textContent = "Blah blabbity blah";
+})
+
+btnY.addEventListener("click", function(){
+    li1.textContent = "Blah blabbity blah";
+    li2.textContent = "Blah blabbity blah";
+    li3.textContent = "Blah blabbity blah";
+    li4.textContent = "Blah blabbity blah";
+})
+
+btnZ.addEventListener("click", function(){
+    li1.textContent = "Blah blabbity blah";
+    li2.textContent = "Blah blabbity blah";
+    li3.textContent = "Blah blabbity blah";
+    li4.textContent = "Blah blabbity blah";
+})
+
